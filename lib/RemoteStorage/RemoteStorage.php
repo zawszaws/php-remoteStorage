6bc13c0fac5d65d004ed16178a319a4061b6abe2
François Kooman <fkooman@tuxed.net>
2012-12-31 10:36:40 +0700

    almost got it working now

--- lib/RemoteStorage/RemoteStorage.php
+++ lib/RemoteStorage/RemoteStorage.php
@@ -4,114 +4,138 @@ namespace RemoteStorage;
 
 use \RestService\Utils\Config as Config;
 use \RestService\Utils\Logger as Logger;
+use \RestService\Http\HttpRequest as HttpRequest;
 use \RestService\Http\HttpResponse as HttpResponse;
+
+use \OAuth\RemoteResourceServer as RemoteResourceServer;
 
 class RemoteStorage
 {
-    private $_c;
-    private $_l;
+    private $_config;
+    private $_logger;
+    private $_rs;
 
-    public function __construct(Config $c, Logger $l)
+    public function __construct(Config $c, Logger $l = NULL)
     {
-        $this->_c = $c;
-        $this->_l = $l;
+        $this->_config = $c;
+        $this->_logger = $l;
+
+        $this->_rs = new RemoteResourceServer($this->_config->getSectionValues("OAuth"));
+        $this->_fs = new FileStorage($this->_config, $this->_logger);
     }
 
-    public function getDir($path)
+    public function handleRequest(HttpRequest $request)
     {
         $response = new HttpResponse(200, "application/json");
+        $response->setHeader("Access-Control-Allow-Origin", "*");
 
-        $entries = array();
-        $dir = realpath($this->_c->getValue('filesDirectory') . $path);
-        if (FALSE !== $dir && is_dir($dir)) {
-            $cwd = getcwd();
-            chdir($dir);
-            foreach (glob("*", GLOB_MARK) as $e) {
-                $entries[$e] = filemtime($e);
+#        $this->_rs->verifyRequest();
+
+        $service = $this->_fs; // FIXME: can this be avoided??
+        $rs = $this->_rs; // FIXME: can this be avoided??
+
+        try {
+            $request->matchRest("OPTIONS", NULL, function() use ($response) {
+                $response->setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin");
+                $response->setHeader("Access-Control-Allow-Methods", "GET, PUT, DELETE");
+            });
+
+            ################
+            # PUBLIC FILES #
+            ################
+
+            // get a file
+            $request->matchRest("GET", "/:user/public/:path+", function($user, $path) use ($request, &$response, $service) {
+                // no auth required
+                $content = $service->getFile($request->getPathInfo());
+                if (FALSE === $content) {
+                    throw new RemoteStorageException("not_found", "file not found");
+                } else {
+                    $response->setContent($content);
+                }
+            });
+
+            // get a directory listing
+            $request->matchRest("GET", "/:user/public/:path+/", function($user, $path) use ($request, &$response, $service) {
+                // auth required
+                $response->setContent($service->getDir(json_encode($request->getPathInfo()), JSON_FORCE_OBJECT));
+            });
+
+            // upload/update a file
+            $request->matchRest("PUT", "/:user/public/:path+", function($user, $path) use ($request, &$response, $service) {
+                // auth required
+                // FIXME: deal with response
+                $service->putFile($request->getPathInfo(), $request->getContent(), $request->getHeader("Content-Type"));
+
+            });
+
+            // delete a file
+            $request->matchRest("DELETE", "/:user/public/:path+", function($user, $path) use ($request, &$response, $service) {
+                // auth required
+                // FIXME: deal with response
+                $service->deleteFile($request->getPathInfo());
+            });
+
+            ####################
+            # NON PUBLIC FILES #
+            ####################
+
+            // get a file
+            $request->matchRest("GET", "/:user/:path+", function($user, $path) use ($request, &$response, $service) {
+                // auth required
+                $response->setContent($service->getFile($request->getPathInfo()));
+            });
+
+            // get a directory listing
+            $request->matchRest("GET", "/:user/:path+/", function($user, $path) use ($request, &$response, $service) {
+                // auth required
+                $response->setContent(json_encode($service->getDir($request->getPathInfo()), JSON_FORCE_OBJECT));
+            });
+
+            // upload/update a file
+            $request->matchRest("PUT", "/:user/:path+", function($user, $path) use ($request, &$response, $service) {
+                // auth required
+                // FIXME: deal with response
+                $service->putFile($request->getPathInfo(), $request->getContent(), $request->getHeader("Content-Type"));
+            });
+
+            // delete a file
+            $request->matchRest("DELETE", "/:user/:path+", function($user, $path) use ($request, &$response, $service) {
+                // auth required
+                // FIXME: deal with response
+                $service->deleteFile($request->getPathInfo());
+            });
+
+            $request->matchRestDefault(function($methodMatch, $patternMatch) use ($request, &$response) {
+                if (in_array($request->getRequestMethod(), $methodMatch)) {
+                    if (!$patternMatch) {
+                        throw new ProxyException("not_found", "resource not found");
+                    }
+                } else {
+                    $response->setResponseCode(405);
+                    $response->setHeader("Allow", implode(", ", $methodMatch));
+                }
+            });
+
+        } catch (RemoteStorageException $e) {
+            $response = new HttpResponse($e->getResponseCode());
+            $response->setHeader("Content-Type", "application/json");
+            $response->setContent(json_encode(array("error" => $e->getMessage(), "error_description" => $e->getDescription())));
+            if (NULL !== $this->_logger) {
+                $this->_logger->logFatal($e->getLogMessage(TRUE) . PHP_EOL . $request . PHP_EOL . $response);
             }
-            chdir($cwd);
-        }
-        $response->setContent(json_encode($entries, JSON_FORCE_OBJECT));
-
-        return $response;
-    }
-
-    public function getFile($path)
-    {
-        $this->_l->logDebug("getting relative file path: " . $path);
-
-        $file = realpath($this->_c->getValue('filesDirectory') . $path);
-        if (FALSE === $file || !is_file($file)) {
-            throw new RemoteStorageException("not_found", "file not found");
-        }
-        if (function_exists("xattr_get")) {
-            $mimeType = xattr_get($file, 'mime_type');
-        } else {
-            $mimeType = "application/json";
-        }
-        $response = new HttpResponse(200, $mimeType);
-
-        $this->_l->logDebug("getting abs file path: " . $file);
-
-        $content = file_get_contents($file);
-        $this->_l->logDebug("file contents: " . $content);
-
-        $response->setContent($content);
-
-        return $response;
-    }
-
-    public function putFile($path, $fileData, $contentType = NULL)
-    {
-        $response = new HttpResponse(200);
-        $file = $this->_c->getValue('filesDirectory') . $path;
-        $directory = dirname($file);
-        $dir = realpath($directory);
-        if (FALSE === $dir) {
-            $this->_createDirectory($directory);
-            $dir = realpath($directory);
-            if (FALSE === $dir) {
-                throw new RemoteStorageException("invalid_request", "unable to create directory");
+        } catch (RemoteResourceServerException $e) {
+            $response = new HttpResponse($e->getResponseCode());
+            $response->setHeader("WWW-Authenticate", $e->getAuthenticateHeader());
+            $response->setHeader("Content-Type", "application/json");
+            $response->setContent($e->getContent());
+            if (NULL !== $this->_logger) {
+                $this->_logger->logWarn($e->getMessage() . PHP_EOL . $e->getDescription() . PHP_EOL . $request . PHP_EOL . $response);
             }
         }
-        if (!is_dir($dir)) {
-            throw new RemoteStorageException("invalid_request", "parent of file already exists and is not a directory");
-        }
-
-        if (NULL === $contentType) {
-            $contentType = "application/json";
-        }
-        file_put_contents($file, $fileData);
-        // store mime_type
-        if (function_exists("xattr_set")) {
-            xattr_set($file, 'mime_type', $contentType);
-        }
 
         return $response;
-    }
 
-    public function deleteFile($path)
-    {
-        $response = new HttpResponse(200);
-        $file = realpath($this->_c->getValue('filesDirectory') . $path);
-        if (FALSE === $file || !is_file($file)) {
-            throw new RemoteStorageException("not_found", "file not found");
-        }
-
-        if (@unlink($file) === FALSE) {
-            throw new RemoteStorageException("internal_server_error", "unable to delete file");
-        }
-
-        return $response;
-    }
-
-    private function _createDirectory($dir)
-    {
-        if (!file_exists($dir)) {
-            if (@mkdir($dir, 0775, TRUE) === FALSE) {
-                throw new Exception("unable to create directory");
-            }
-        }
     }
 
 }
--- www/api.php
+++ www/api.php
@@ -9,10 +9,6 @@ use \RestService\Utils\Config as Config;
 use \RestService\Utils\Logger as Logger;
 
 use \RemoteStorage\RemoteStorage as RemoteStorage;
-use \RemoteStorage\RemoteStorageException as RemoteStorageException;
-
-use \OAuth\RemoteResourceServer as RemoteResourceServer;
-use \OAuth\RemoteResourceServerException as RemoteResourceServerException;
 
 $logger = NULL;
 $request = NULL;
@@ -22,106 +18,14 @@ try {
     $config = new Config(dirname(__DIR__) . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "remoteStorage.ini");
     $logger = new Logger($config->getSectionValue('Log', 'logLevel'), $config->getValue('serviceName'), $config->getSectionValue('Log', 'logFile'), $config->getSectionValue('Log', 'logMail', FALSE));
 
-    $rs = new RemoteResourceServer($config->getSectionValues("OAuth"));
-    //$rs->verifyRequest();
-
+    $remoteStorage = new RemoteStorage($config, $logger);
     $request = HttpRequest::fromIncomingHttpRequest(new IncomingHttpRequest());
+    $response = $remoteStorage->handleRequest($request);
 
-    $response = new HttpResponse(200, "application/json");
-    $response->setHeader("Access-Control-Allow-Origin", "*");
-
-    $service = new RemoteStorage($config, $logger);
-
-    $request->matchRest("OPTIONS", NULL, function() use ($response) {
-        $response->setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin, If-None-Match, If-Match");
-        $response->setHeader("Access-Control-Allow-Methods", "GET, PUT, DELETE");
-    });
-
-    ################
-    # PUBLIC FILES #
-    ################
-
-    // get a file
-    $request->matchRest("GET", "/:user/public/:path+", function($user, $path) use ($request, &$response, $service) {
-        // no auth required
-        $response = $service->getFile($request->getPathInfo());
-    });
-
-    // get a directory listing
-    $request->matchRest("GET", "/:user/public/:path+/", function($user, $path) use ($request, &$response, $service) {
-        // auth required
-        $response = $service->getDir($request->getPathInfo());
-    });
-
-    // upload/update a file
-    $request->matchRest("PUT", "/:user/public/:path+", function($user, $path) use ($request, &$response, $service) {
-        // auth required
-        $response = $service->putFile($request->getPathInfo(), $request->getContent(), $request->getHeader("Content-Type"));
-    });
-
-    // delete a file
-    $request->matchRest("DELETE", "/:user/public/:path+", function($user, $path) use ($request, &$response, $service) {
-        // auth required
-        $response = $service->deleteFile($request->getPathInfo());
-    });
-
-    ####################
-    # NON PUBLIC FILES #
-    ####################
-
-    // get a file
-    $request->matchRest("GET", "/:user/:path+", function($user, $path) use ($request, &$response, $service) {
-        // auth required
-        $response = $service->getFile($request->getPathInfo());
-    });
-
-    // get a directory listing
-    $request->matchRest("GET", "/:user/:path+/", function($user, $path) use ($request, &$response, $service) {
-        // auth required
-        $response = $service->getDir($request->getPathInfo());
-    });
-
-    // upload/update a file
-    $request->matchRest("PUT", "/:user/:path+", function($user, $path) use ($request, &$response, $service) {
-        // auth required
-        $response = $service->putFile($request->getPathInfo(), $request->getContent(), $request->getHeader("Content-Type"));
-    });
-
-    // delete a file
-    $request->matchRest("DELETE", "/:user/:path+", function($user, $path) use ($request, &$response, $service) {
-        // auth required
-        $response = $service->deleteFile($request->getPathInfo());
-    });
-
-    $request->matchRestDefault(function($methodMatch, $patternMatch) use ($request, &$response) {
-        if (in_array($request->getRequestMethod(), $methodMatch)) {
-            if (!$patternMatch) {
-                throw new ProxyException("not_found", "resource not found");
-            }
-        } else {
-            $response->setResponseCode(405);
-            $response->setHeader("Allow", implode(", ", $methodMatch));
-        }
-    });
-
-} catch (RemoteStorageException $e) {
-    $response = new HttpResponse($e->getResponseCode());
-    $response->setHeader("Content-Type", "application/json");
-    $response->setContent(json_encode(array("error" => $e->getMessage(), "error_description" => $e->getMessage())));
-    if (NULL !== $logger) {
-        $logger->logFatal($e->getLogMessage(TRUE) . PHP_EOL . $request . PHP_EOL . $response);
-    }
-} catch (RemoteResourceServerException $e) {
-    $response = new HttpResponse($e->getResponseCode());
-    $response->setHeader("WWW-Authenticate", $e->getAuthenticateHeader());
-    $response->setHeader("Content-Type", "application/json");
-    $response->setContent($e->getContent());
-    if (NULL !== $logger) {
-        $logger->logWarn($e->getMessage() . PHP_EOL . $e->getDescription() . PHP_EOL . $request . PHP_EOL . $response);
-    }
 } catch (Exception $e) {
-    $response = new HttpResponse(500);
-    $response->setHeader("Content-Type", "application/json");
+    // any other error thrown by any of the modules, assume internal server error
+    $response = new HttpResponse();
+    $response->setStatusCode(500);
     $response->setContent(json_encode(array("error" => "internal_server_error", "error_description" => $e->getMessage())));
     if (NULL !== $logger) {
         $logger->logFatal($e->getMessage() . PHP_EOL . $request . PHP_EOL . $response);

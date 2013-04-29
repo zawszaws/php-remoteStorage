02f09758245b7286cfe7a0e0b76a904e04f933c5
François Kooman <fkooman@tuxed.net>
2013-04-29 13:14:59 +0200

    deal with not set realm config parameter

--- lib/RemoteStorage/RemoteStorage.php
+++ lib/RemoteStorage/RemoteStorage.php
@@ -199,7 +199,7 @@ class RemoteStorage
                 $this->_logger->logFatal($e->getLogMessage(TRUE) . PHP_EOL . $request . PHP_EOL . $response);
             }
         } catch (RemoteResourceServerException $e) {
-            $e->setRealm($this->_config->getSectionValue("OAuth", "realm"));
+            $e->setRealm($this->_config->getSectionValue("OAuth", "realm", FALSE));
             $response = new HttpResponse($e->getResponseCode());
             $response->setHeader("WWW-Authenticate", $e->getAuthenticateHeader());
             $response->setHeader("Content-Type", "application/json");
